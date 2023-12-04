613004c42f9ede5904e9cd6222b8dd97f2714349
EsraaKandil <152459472+EsraaKandil@users.noreply.github.com>
2023-12-04 03:14:39 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+let img = document.querySelector('.img');
+let container = document.querySelector('.container');
+
+
+function flowers(flower){
+
+    img.src = flower;
+
+}
+function colors(color){
+    container .Style.background = color ;
+
+}
+let names = document.getElementById("last name").value;
+let email = document.getElementById("email").value;
+let massage = document.getElementById("massage").value;
+let ok = document.getElementById("ok").value;
+let  sentbtn= document.getElementById(" sentbtn").value;
+let popup = document.getElementById("ok").value;
+
